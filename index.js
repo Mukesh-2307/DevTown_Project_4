90ef4308c054c3d210a1ee5d1da9e0192b83b8e1
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-08-29 08:17:33 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const router = express.Router();
 const dotenv = require("dotenv");
 const DbConnection = require("./DbConnect");
+const {users, books} = require("./models/index.js");
 
+const userRouter = require("./routes/users.js");
+const booksRouter = require("./routes/books.js");
 dotenv.config();
 
 const app = express();
@@ -11,6 +15,17 @@ DbConnection();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use("/books",booksRouter);
+app.use("/users",userRouter);
+
+app.get("*",(req,res)=>{
+    return res.status(404).json({
+        success: false,
+        message: "route doesn't exist !"
+    })
+})
+
 
 app.listen(PORT,()=>{
     console.log(`app is listening at port : ${PORT}`);
