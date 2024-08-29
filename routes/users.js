56f1346cdba7045f310b2494a7d4b572255e5070
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-08-29 08:18:34 +0000

    updated routes for users

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 const {userModel,bookModel} = require("../models/index.js");
+
+// import all the routes from user controller 
+
+// link the imported routes with the desired routes names
+
+module.exports = router;
